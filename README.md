da0b53c4489feda2c1642a684fa990fe8b335475
liliya-tagirova <tor-ver@mail.ru>
2022-08-14 19:25:29 +0300

    feat: edit docker and docker-compose

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Time application consists of the frontend and backend parts
 Frontend is written with help of the Vue.js framework
 Backend is written using Node.js and Express
 Database is MySQL
+
+Сan save data to mysql database from one container to another
+Сan edit the code when the container is raised
--- api/src/utils/mysqlPool.mjs
+++ api/src/utils/mysqlPool.mjs
@@ -1,12 +1,20 @@
 import mysql from 'mysql2'
 
+const MYSQL_HOST = process.env.MYSQL_HOST || 'mysql'
+const MYSQL_USER = process.env.MUSQL_HOST || 'root'
+const MYSQL_PORT = process.env.MUSQL_PORT || '3306'
+const MYSQL_PASSWORD = process.env.MUSQL_PASSWORD || 'password'
+const MYSQL_DB = process.env.MUSQL_DB || 'admin'
+
+// console.log(process.env.MYSQL_HOST)
+
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'mysql',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'time_db',
+  host: 'MYSQL_HOST',
+  port: 'MYSQL_PORT',
+  user: 'MYSQL_USER',
+  password: 'MYSQL_PASSWORD',
+  database: 'MYSQL_DB',
 })
 
 const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
